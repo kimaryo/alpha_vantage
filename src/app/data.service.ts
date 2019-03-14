3d268f676e4c2d6561503b93801a8fbdb8f97b66
Fredric Billow Lunicore <fredric.billow@lunicore.se>
2019-03-14 15:46:17 +0100

    Searching for stocks now working

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -12,6 +12,7 @@ export class DataService {
   api_key: string = "F8EATDHYRIQMFR0N";
   url_daily =
     "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&";
+  url_search = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&";
 
   constructor(private http: HttpClient) {}
 
@@ -22,6 +23,18 @@ export class DataService {
   getData(): Observable<any[]> {
     return this.http
       .get(this.url_daily + `symbol=${"MSFT"}&apikey=${this.api_key}`)
+      .pipe(
+        map(response => {
+          return response as any;
+        })
+      );
+  }
+
+  searchForStock(input): Observable<any[]> {
+    return this.http
+      .get(this.url_search + `keywords=${input}&apikey=${this.api_key}`)
+      //test query, not to waste our 500 request per day
+      //.get("https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=Micro&apikey=demo")
       .pipe(
         map(response => {
           return response as any;
--- src/app/searchbar/searchbar.component.ts
+++ src/app/searchbar/searchbar.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 //import { searchservice } from "../searchservice.service";
+import { DataService } from "../data.service";
+import { switchMap } from 'rxjs/operators';
 
 import {Observable} from 'rxjs';
 import {debounceTime, distinctUntilChanged, map} from 'rxjs/operators';
+import { AnyFn } from '@ngrx/store/src/selector';
+import { shallowEqualArrays } from '@angular/router/src/utils/collection';
+import {CompanyComponent } from '../company/company.component';
 
 const states = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California', 'Colorado',
   'Connecticut', 'Delaware', 'District Of Columbia', 'Federated States Of Micronesia', 'Florida', 'Georgia',
@@ -20,23 +25,67 @@ const states = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'C
 })
 
 export class SearchbarComponent implements OnInit {
-
+   model: String;
+   currentData = [];
+   arrayDisplayStocks = [];
+   testhtArray = [{test: 1}, {test: 2}]
   constructor(
-
+    private data: DataService,
   ) { }
 
   ngOnInit() {
 
   }
-  public model: any;
+
+   searchStocks(input): Observable<any[]> {
+     return this.data.searchForStock(input).pipe(map(response => {
+       let tempArray = [];
+       let extracted = response["bestMatches"];
+       this.currentData = extracted;
+      for(let i = 0; i < extracted.length; i+=1) {
+        tempArray.push(`${i+1}. ${extracted[i]["2. name"]} (${extracted[i]["1. symbol"]})`);
+      }
+       return tempArray;
+     }));
+  }
+
+  keyUpFunction(event) {
+    if(event.keyCode == 13) {
+      if(this.model === "") {
+        return;
+      } else {
+        //alert('you just clicked enter');
+        let temp = this.model.slice(0,1);
+        let index = Number.parseInt(temp) - 1;
+        //this.model = this.model.slice(3, this.model.length);
+        this.arrayDisplayStocks.push(this.currentData[index])
+        for(let i = 0; i < this.arrayDisplayStocks.length; i+=1) {
+          console.log(`${i+1}. ${this.arrayDisplayStocks[i]["2. name"]} (${this.arrayDisplayStocks[i]["1. symbol"]})`);
+        }
+        console.log(this.arrayDisplayStocks)
+      }
+    }
+  }
+
+  // public model: String;
+  // public currentData = [];
+  // public arrayDisplayStocks = [];
 
   formatter = (result: string) => result.toUpperCase();
 
-  search = (text$: Observable<string>) =>
+  search = (text$: Observable<string>) => 
     text$.pipe(
       debounceTime(200),
       distinctUntilChanged(),
-      map(term => term === '' ? []
-        : states.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
+      //map(term => term === '' ? [] : states.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
+        //map(term => term === '' ? [] : this.searchStocks(term).subscribe(res => { console.log(res); res.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10)}))
+        //map(term => term === '' ? [] : this.testfunc(term))
+        switchMap(term => term === '' ? [] : this.searchStocks(term)
+          .pipe(
+            map(results => results.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10)) 
+          )
+        )
+
     )
+  
 }
